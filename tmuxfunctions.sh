5490fd416b508f7b012c389f54c4406a42c254ba
Sven Minning <sven.minning@consultant.volvo.com>
2020-02-21 15:25:34 +0100

    Added tmuxfunctions.sh to be sourced from .bashrc

---
+++ tmuxfunctions.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+#
+# C-b q displays pane-numbers
+#
+# Expected to be run from within tmux
+# Setup a workspace for RESODOS
+# Top panels with resodos scripts
+# Bottom open logs
+function rswdl () {
+    # Open 3 panes
+    tmux3;
+    # Goto commands
+    tmux send-keys "goto rswdl-server" C-m;
+    tmux select-pane -R;
+    tmux send-keys "goto rswdl-server" C-m;
+    # Goto logs
+    tmux select-pane -D;
+    tmux send-keys "tf /vehicleservices/products/thirdparty/jboss/jboss-eap-6.4-vehicle-services-rswdl-server/log/server.log" C-m;
+    # Select pane 0
+    tmux select-pane -t 0;
+
+}
+
+function tmux3() {
+    # Open 3 windows, end in top left #0, top right #1, bottom #2
+    if ! __checkTmux -eq 1; then
+       echo "Are you in tmux?"
+       return;
+    fi
+    tmux split-window -v;
+    tmux resize-pane -D 15;
+    tmux select-pane -U;
+    tmux split-window -h;
+    tmux select-pane -t 0;
+}
+
+function tmux4() {
+    # Open 4 windows, end in top left #0, top right #1, bottom left #2, bottom right #3
+    if ! __checkTmux -eq 1; then
+       echo "Are you in tmux?"
+       return;
+    fi
+    tmux split-window -v;
+    tmux split-window -h;
+    tmux select-pane -U;
+    tmux split-window -h;
+    tmux select-pane -t 0;
+}
+
+function __checkTmux() {
+    if ! { [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; } then
+       return 1
+    fi
+    return 0
+}
